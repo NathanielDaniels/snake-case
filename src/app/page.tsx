df93a58126077230d68944a0e3af535dae4c85d7
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2024-05-17 18:37:10 -0500

    updated first page with customer review section and added icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
 import { Check, Star } from "lucide-react";
+import { Icons } from "@/components/Icons";
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="bg-slate-50">
+    <div className="bg-slate-50 grainy-light">
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 x1:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
@@ -22,12 +21,12 @@ export default function Home() {
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Capture your favorite memories with your own,{" "}
-                <span className="font-bold">one-of-one</span> phone case.
+                <span className="font-semibold">one-of-one</span> phone case.
                 CaseCobra allows you to protect your memories, not just your
                 phone.
               </p>
               <div className="space-y-2">
-                <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <ul className="mt-8 mb-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="w-5 h-5 shrink-0 text-green-600" />
                     High-quality, durable material
@@ -86,7 +85,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="col-span-full lg:col-span-1 w-full flex justify-centerd px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit ">
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit ">
             <div className="relative md:max-w-x1">
               <img
                 src="/your-image.png"
@@ -100,6 +99,25 @@ export default function Home() {
               />
               <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{" "}
+              say
+            </h2>
+            <img
+              src="/snake-2.png"
+              className="w-24 order-0 lg:order-2"
+              alt="snake"
+            />  
           </div>
         </MaxWidthWrapper>
       </section>
