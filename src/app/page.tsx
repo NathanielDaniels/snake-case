807e7a6d7aeeb9c4bc1f0b35c276cc48ae27581a
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2024-05-18 16:31:23 -0500

    added small margin-left to main page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 x1:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
-            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+            <div className="relative mx-auto md:ml-10 text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
                 <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28" />
                 <img className="w-full" src="/snake-1.png" alt="snake logo" />
@@ -117,7 +117,7 @@ export default function Home() {
               src="/snake-2.png"
               className="w-24 order-0 lg:order-2"
               alt="snake"
-            />  
+            />
           </div>
         </MaxWidthWrapper>
       </section>
