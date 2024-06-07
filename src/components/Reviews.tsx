4f70db8b6524c7e09b993bf6c90220e6c22422de
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2024-06-07 18:38:22 -0500

    animated phone scroll section

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,7 +1,10 @@
-import { HTMLAttributes, useEffect, useRef, useState } from "react";
+"use client";
+
+import { useEffect, useRef, useState, HTMLAttributes } from "react";
 import { MaxWidthWrapper } from "./MaxWidthWrapper";
 import { useInView } from "framer-motion";
 import { cn } from "@/lib/utils";
+import Phone from "./Phone";
 
 const PHONES = [
   "/testimonials/1.jpg",
@@ -34,22 +37,19 @@ function ReviewColumn({
 }: {
   reviews: string[];
   className?: string;
-  reviewClassName?: (index: number) => string;
+  reviewClassName?: (reviewIndex: number) => string;
   msPerPixel?: number;
 }) {
   const columnRef = useRef<HTMLDivElement | null>(null);
   const [columnHeight, setColumnHeight] = useState(0);
   const duration = `${columnHeight * msPerPixel}ms`;
+
   useEffect(() => {
     if (!columnRef.current) return;
 
-    const resizeObserver = new window.ResizeObserver((entries) => {
+    const resizeObserver = new window.ResizeObserver(() => {
       setColumnHeight(columnRef.current?.offsetHeight ?? 0);
     });
-
-    const column = columnRef.current;
-    const columnHeight = column.getBoundingClientRect().height;
-    setColumnHeight(columnHeight);
 
     resizeObserver.observe(columnRef.current);
 
@@ -63,7 +63,48 @@ function ReviewColumn({
       ref={columnRef}
       className={cn("animate-marquee space-y-8 py-4", className)}
       style={{ "--marquee-duration": duration } as React.CSSProperties}
-    ></div>
+    >
+      {reviews.concat(reviews).map((imgSrc, reviewIndex) => (
+        <Review
+          key={reviewIndex}
+          className={reviewClassName?.(reviewIndex % reviews.length)}
+          imgSrc={imgSrc}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+}
+
+function Review({ imgSrc, className, ...props }: ReviewProps) {
+  const POSSIBLE_ANIMATION_DELAYS = [
+    "0s",
+    "0.1s",
+    "0.2s",
+    "0.3s",
+    "0.4s",
+    "0.5s",
+  ];
+
+  const animationDelay =
+    POSSIBLE_ANIMATION_DELAYS[
+      Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
+    ];
+
+  return (
+    <div
+      className={cn(
+        "animate-fade-in rounded-[2.25rem] bg-white p-6 opacity-0 shadow-xl shadow-slate-900/5",
+        className
+      )}
+      style={{ animationDelay }}
+      {...props}
+    >
+      <Phone imgSrc={imgSrc} />
+    </div>
   );
 }
 
@@ -78,7 +119,7 @@ function ReviewGrid() {
   const column3 = splitArray(columns[2], 2);
   return (
     <div
-      //   ref={containerRef}
+      ref={containerRef}
       className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
     >
       {isInView ? (
@@ -117,14 +158,12 @@ function ReviewGrid() {
 export function Reviews() {
   return (
     <MaxWidthWrapper className="relative max-w-5xl">
-      <></>
-      {/* <img
+      <img
         aria-hidden="true"
         src="/what-people-are-buying.png"
         className="absolute select-none hidden xl:block -left-32 top-1/3"
-      /> */}
-
-      {/* <ReviewGrid /> */}
+      />
+      <ReviewGrid />
     </MaxWidthWrapper>
   );
 }
