a9c2881fbe5d05bcf9feb840254ebf601fcd5bd8
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2024-08-22 13:24:05 -0500

    started configure page with cuztomize section (part 2/3)

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,12 +1,37 @@
-interface PageProps {
-    searchParams: {
-        [key: string]: string | string[] | undefined;
-    };
-}   
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
 
-const Page = async ({searchParams}: PageProps) => {
-    const {id} = searchParams;
-    return <p>{id}</p>
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 }
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, height, width } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={id}
+      imageUrl={imageUrl}
+      imageDimensions={{ height, width }}
+    />
+  );
+};
 
 export default Page;
