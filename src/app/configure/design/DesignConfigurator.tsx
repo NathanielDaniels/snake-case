7fda7c7b2d4a3f9bdaaf3da97a731ab73356711d
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2024-08-23 23:22:13 -0500

    Added Aspect-Ratio and React-RND to the Design Configure page

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,6 +1,9 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio";
-import NextImage from "next/image";
+"use client";
 
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { cn } from "@/lib/utils";
+import NextImage from "next/image";
+import { Rnd } from "react-rnd";
 interface DesignConfiguratorProps {
   configId: string;
   imageUrl: string;
@@ -16,7 +19,7 @@ const DesignConfigurator = ({
   imageDimensions: { height, width },
 }: DesignConfiguratorProps) => {
   return (
-    <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
+    <div className="relative mt-20 mb-20 pb-20 flex justify-center items-center">
       <div
         // ref={containerRef}
         className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
@@ -33,7 +36,32 @@ const DesignConfigurator = ({
               className="pointer-events-none z-50 select-none"
             />
           </AspectRatio>
+          <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]" />
+          <div
+            className={cn(
+              "absolute insert-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
+              "bg-zinc-950"
+            )}
+          />
         </div>
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: height / 4,
+            width: width / 4,
+          }}
+        >
+          <div className="relative w-full h-full">
+            <NextImage
+              src={imageUrl}
+              fill
+              alt="your image"
+              className="pointer-events-none"
+              objectFit="contain"
+            />
+          </div>
+        </Rnd>
       </div>
     </div>
   );
