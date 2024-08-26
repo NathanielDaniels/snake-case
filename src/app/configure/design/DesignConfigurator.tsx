6686aa8edcc96a7ee0baaec6d652995a444ee520
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2024-08-25 23:21:55 -0500

    added scroll-area and radio-group

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -5,6 +5,10 @@ import { cn } from "@/lib/utils";
 import NextImage from "next/image";
 import { Rnd } from "react-rnd";
 import HandleComponent from "@/components/HandleComponent";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { RadioGroup } from "@headlessui/react";
+import { useState } from "react";
+
 interface DesignConfiguratorProps {
   configId: string;
   imageUrl: string;
@@ -19,6 +23,7 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimensions: { height, width },
 }: DesignConfiguratorProps) => {
+  const [options, setOptions] = useState<Record<string, string>>({color: "black"});
   return (
     <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
       <div
@@ -71,6 +76,23 @@ const DesignConfigurator = ({
             />
           </div>
         </Rnd>
+      </div>
+      <div className="h-[37.5rem] flex flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"
+          />
+          <div className="px-8 pb-12 bt-8">
+            <h2 className="tracking-tight font-bold test-3x1">
+              Customize Your Case
+            </h2>
+            <div className="w-full h-px bg-zinc-200 my-6" />
+            <div className="relative mt-4 h-full flex flex-col justify-between">
+              <RadioGroup value=""></RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
