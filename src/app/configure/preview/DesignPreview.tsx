b48c444047752913248dec21f0c8602898686b0b
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2024-09-23 23:31:48 -0500

    added highlights and materials information to summary

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -5,6 +5,8 @@ import { Configuration } from "@prisma/client";
 import Phone from "@/components/Phone";
 import { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
+import { COLORS, MODELS } from "@/validators/option-validator";
+import { Check } from "lucide-react";
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   const config = {
     // angle: 90,
@@ -25,13 +27,15 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
   useEffect(() => setShowConfetti(true));
 
-  console.log(configuration);
+  const { color, model, finish, material } = configuration;
 
-  // const { color, model, finish, material } = configuration;
+  const tw = COLORS.find(
+    (supportedColor) => supportedColor.value === color
+  )?.tw;
 
-  //   const tw = COLORS.find(
-  //     (supportedColor) => supportedColor.value === color
-  //   )?.tw;
+  const { label: modelLabel } = MODELS.options.find(
+    ({ value }) => value === model
+  )!;
   return (
     <>
       <div
@@ -44,9 +48,44 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
       <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
           <Phone
-            // className={cn(`bg-${tw}`, "max-w-[150px] md:max-w-full")}
+            className={cn(`bg-${tw}`, "max-w-[150px] md:max-w-full")}
             imgSrc={configuration.croppedImageUrl!}
           />
+        </div>
+        <div className="mt-6 sm:col-span-9 sm:mt-0 md:row-end-1">
+          <h3 className="text-3xl font-bold tracking-tight text-gray-900">
+            Your {modelLabel} Case
+          </h3>
+          <div className="mt-3 flex item-center gap-1.5 text-base">
+            <Check className="h-4 w-4 text-green-500" />
+            In stock and ready to ship
+          </div>
+        </div>
+
+        <div className="sm:col-span-12 md:col-span-9 text-base">
+          <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+            <div>
+              <p className="font-medium text-zinc-950">Highlights</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>Wireless charging compatible</li>
+                <li>Made from recycled materials</li>
+                <li>5 year print warranty</li>
+              </ol>
+            </div>
+            <div>
+              <p className="font-medium text-zinc-950">Materials</p>
+              <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                <li>High-quality {material} material</li>
+                <li>Scratch & fingerprint resistant {finish} finish</li>
+                {/* <li>Color: {color}</li> */}
+              </ol>
+            </div>
+          </div>
+          <div className="mt-8">
+            <div className="bg-gray-50 p-6 sm:rounded-lg sm:p-8">
+              <div className="flow-root"></div>
+            </div>
+          </div>
         </div>
       </div>
     </>
