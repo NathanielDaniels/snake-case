90d046cdf4f7b4e09c6b0417657f20a1bef73f3e
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2025-01-14 23:59:02 -0600

    building Thank you component

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getPaymentStatus } from "./actions";
 import { useSearchParams } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import PhonePreview from "@/components/PhonePreview";
 
 const ThankYou = () => {
   const searchParams = useSearchParams();
@@ -16,34 +17,72 @@ const ThankYou = () => {
     retryDelay: 500,
   });
 
-    if (data === undefined) {
-      return (
-        <div className="w-full mt-24 flex justify-center">
-          <div className="flex flex-col items-center gap-2">
-            <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
-            <h3 className="font-semibold text-xl">Loading your order...</h3>
-            <p>This won't take long.</p>
-          </div>
+  if (data === undefined) {
+    return (
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+          <h3 className="font-semibold text-xl">Loading your order...</h3>
+          <p>This won't take long.</p>
         </div>
-      );
-    }
+      </div>
+    );
+  }
 
-    if (data === false) {
-      return (
-        <div className="w-full mt-24 flex justify-center">
-          <div className="flex flex-col items-center gap-2">
-            <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
-            <h3 className="font-semibold text-xl">Verifying your payment...</h3>
-            <p>This might take a moment.</p>
+  if (data === false) {
+    return (
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+          <h3 className="font-semibold text-xl">Verifying your payment...</h3>
+          <p>This might take a moment.</p>
+        </div>
+      </div>
+    );
+  }
+
+  const { configuration, billingAddress, shippingAddress, amount } = data;
+  const { color } = configuration;
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-3xl px-4 py-16 sm:py-24 lg:px-8">
+        {/* <h1>Thank you for your purchase!</h1>
+      <p>You will receive an email confirmation shortly.</p> */}
+        <div className="max-w-xl">
+          <p className="text-base font-medium text-primary">Thank you!</p>
+          <h1 className="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">
+            Your case is on the way!
+          </h1>
+          <p className="mt-2 text-base text-zinc-500">
+            We've received your order and is now processing.
+          </p>
+
+          <div className="mt-12 text-sm font-medium">
+            <p className="text-zinc-900">Order number</p>
+            <p className="mt-2 text-zinc-500">{orderId}</p>
           </div>
         </div>
-      );
-    }
-    
-  return (
-    <div>
-      <h1>Thank you for your purchase!</h1>
-      <p>You will receive an email confirmation shortly.</p>
+        <div className="mt-10 border-t border-zinc-200">
+          <div className="mt-10 flex flex-auto flex-col">
+            <h4 className="font-semibold text-zinc-900">
+              You made a great choice!
+            </h4>
+            <p className="mt-2 text-sm text-zinc-600">
+              At CobraCase, we believe a phone case should do more than just
+              look great—it should stand the test of time. That’s why we offer a
+              5-year print guarantee. If your case doesn’t meet our high-quality
+              standards, we’ll replace it free of charge.
+            </p>
+          </div>
+        </div>
+        <div className="flex space-x-6 overflow-hidden mt-4 rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
+          <PhonePreview
+            croppedImageUrl={configuration.croppedImageUrl!}
+            color={color!}
+          />
+        </div>
+      </div>
     </div>
   );
 };
